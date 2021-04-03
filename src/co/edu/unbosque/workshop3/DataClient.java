d9f0bca8cc1e9d79f921de10e45d16bdb3260b2c
Camilo Gomez <fcgomez@unbosque.edu.co>
2021-04-02 19:00:49 -0500

    Creando la clase DataClient

--- src/co/edu/unbosque/workshop3/DataClient.java
+++ src/co/edu/unbosque/workshop3/DataClient.java
@@ -1,4 +1,33 @@
 package co.edu.unbosque.workshop3;
 
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class DataClient {
+
+    public static void main(String[] args) throws Exception {
+
+        try (var socket = new Socket("127.0.0.1", 59897)) {
+
+            System.out.println("Connected: " + socket);
+//            System.out.println("Enter the message to be capitalized...");
+
+            var scanner = new Scanner(System.in);
+            var in = new Scanner(socket.getInputStream());
+            var out = new PrintWriter(socket.getOutputStream(), true);
+           
+            System.out.println(in.nextLine());
+            
+            String mensaje = "";
+            while (scanner.hasNextLine()) {
+            	mensaje = scanner.nextLine();
+                out.println(mensaje);
+                System.out.println(in.nextLine());
+            }
+
+        }
+
+    }
+
 }
