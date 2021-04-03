c8a19d020204dcf7c5299113a5c715a2fdaa59f4
Camilo Gomez <fcgomez@unbosque.edu.co>
2021-04-02 19:19:46 -0500

    Creando la clase DataAgent

--- src/co/edu/unbosque/workshop3/DataAgent.java
+++ src/co/edu/unbosque/workshop3/DataAgent.java
@@ -1,4 +1,32 @@
 package co.edu.unbosque.workshop3;
 
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class DataAgent {
+
+    public static void main(String[] args) throws Exception {
+
+        try (var socket = new Socket("127.0.0.1", 59897)) {
+
+            System.out.println("Connected: " + socket);
+
+            var scanner = new Scanner(System.in);
+            var in = new Scanner(socket.getInputStream());
+            var out = new PrintWriter(socket.getOutputStream(), true);
+           
+            System.out.println(in.nextLine());
+            
+            String mensaje = "";
+            while (scanner.hasNextLine()) {
+            	mensaje = scanner.nextLine();
+                out.println(mensaje);
+                System.out.println(in.nextLine());
+            }
+
+        }
+
+    }
+
 }
